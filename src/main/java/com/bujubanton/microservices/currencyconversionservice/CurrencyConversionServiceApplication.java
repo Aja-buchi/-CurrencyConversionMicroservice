571b9db31b08a5bd00939263e548aa775f9096b1
DESKTOP-EL54GVD\USER <onyebuchiaja@gmail.com>
2023-03-30 11:48:43 +0100

    "feign client dependency error rectified"

--- src/main/java/com/bujubanton/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/bujubanton/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,10 @@ package com.bujubanton.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClient
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
